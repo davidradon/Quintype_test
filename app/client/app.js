3572ec758ab9d42ea6c9b6f817e1d0fea4073ccf
unknown <davidradon@gmail.com>
2018-06-18 15:14:01 +0530

    second commit, bugs fixed

--- app/client/app.js
+++ app/client/app.js
@@ -3,7 +3,12 @@ global.startApp = function(container) {
     var box = document.getElementsByClassName('cell');
     let diamondArr = [];
     let score = 64;
+    let numberArr = [];
+    let foundDiamonds = [];
 
+    for(let i=0;i<64;i++){
+        numberArr.push(i);
+    }
 
     for (var i = 0; i < box.length; i++) {
         box[i].addEventListener('click', flipSide);
@@ -16,22 +21,23 @@ global.startApp = function(container) {
     function getDiamondArray() {
         var totalDiamond = 8;
 
+            for (let i = 0,maxTotal=63 ; i < totalDiamond; i++,maxTotal--) {
+                var diamond = Math.floor(Math.random() * maxTotal)+1;
+                diamondArr.push('sq-'+ numberArr[diamond]);
+                numberArr.splice(diamond,1);
 
-        for (var i = 0; i < totalDiamond; i++) {
-            var diamond = Math.floor(Math.random() * 64);
-            if (diamondArr.indexOf(diamond) == -1) {
-                diamondArr.push('sq-'+diamond);
-                continue;
             }
 
-        }
+        console.log(diamondArr);
     }
 
     function flipSide(e) {
         var touched = e.target.id;
+
         if(diamondArr.indexOf(touched) != -1){
 
             document.getElementById(touched).classList.add('diamond');
+            foundDiamonds.push(touched);
         }
         else{
             score = score-1;
@@ -39,6 +45,11 @@ global.startApp = function(container) {
         }
         document.getElementById(touched).classList.remove('unknown');
         document.getElementById(touched).removeEventListener('click',flipSide);
+        console.log(foundDiamonds.length);
+        if(foundDiamonds.length == 8){
+            document.getElementById('message').innerHTML = "You've found all the diamonds!!"
+
+        }
 
     }
 
